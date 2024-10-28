40007c8f3cc95d95a259f527c9d2efb9ce0c602c
souley-dot <souleymane010197@gmail.com>
2024-10-28 09:13:48 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,236 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <link rel="shortcut icon" href="logo.ico">
+    <title>Cité de la diaspora</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500"> 
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/mediaelementplayer.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/fl-bigmug-line.css">
+  
+    <link rel="stylesheet" href="css/aos.css">
+    
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--Rajout de css-->
+    <!--<link rel="stylesheet" href="css/style-2.css">-->
+    
+  </head>
+  <body>
+
+  <div class="site-loader container"></div>
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+    
+    
+    </div>
+    <div class="container">
+      <div class="row p-2">
+
+      <a href="physalis.php"><img  src="images/Logo-Fimolux-new.png" alt="logo" title="Logo-fimolux"></a>
+      <a href="Maison.php"><img  src="images/logo phylasis-new.png" alt="logo" title="phylasis"></a>
+
+      </div>
+
+
+
+    </div>
+    
+    <!--Début menu perso-->
+    
+    <div class="container-fluid justify-content-center  bg-dark bg-opacity-50">
+
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon text-white">Menu</span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup"->
+      <div class="navbar-nav">
+        <a class="nav-link text-white" aria-current="page" href="#">Acueil</a>
+        <a class="nav-link text-white" href="physalis.php">Fimolux</a>
+        <a class="nav-link text-white" href="Maison.php">SRT Physalis</a>
+        <a class="nav-link text-white" href="Affiche_type.php?id=15&id2=125">Nos logements</a>
+        <a class="nav-link text-white" href="ajout_reservation.php">Réserver</a>
+        <a class="nav-link text-white" href="../page_etp.php">Promoteurs</a>
+        <a class="nav-link text-white" href="#">Qui-sommes-nous</a>
+      
+      </div>
+    </div>
+  </div>
+</nav>
+</div>
+
+<!--fin de menu-->
+<!--images d'accuiel perso-->
+<div class="container-fluid mt-3 mb-5 site-blocks-cover inner-page-cover overlay " style="background-image:url(images/cdc8.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+
+      <div class="container mt-3">
+        <div class="row  align-items-center justify-content-center text-center">
+          <div class="col-md-7">
+            <span class="d-inline-block text-white px-3 mb-3 property-offer-type rounded"></span>
+            <h1 class="mb-2 text-white font-weight-bold">Bienvenue à la cité de la diaspora du Fongad Invest</h1>
+            <!--
+            <p class="mb-5 d-hidden"><strong class="h2 text-white">Les propriétaires bénéficieront du confort et de la quiétude auxquels ils aspirent tant au niveau de la qualité de construction qu’à celui de la sécurité.
+            La Cite de la Diaspora va à être aménagée dans un cadre autonome de tranquillité et de sérénité avec toutes les commodités nécessaires...</strong></p>
+           -->
+            
+            <a href="physalis.php" class="text-white p-4"><button type="button" class="btn  bg-dange text-white btn-outline-danger ">Je découvre</button></a>
+           <!-- <a href="physalis.php" class="text-black p-2"><button type="button" class="btn btn-outline-success">Je découvre</button></a>-->
+          </div>
+        </div>
+      </div>
+      
+    </div>
+
+    <!--fin d'accuiel-->
+    <aside>
+
+       <div class="container mb-4">
+        <div class="row bg-body-tertiary">
+          <div class="col-md-3 mb-2 col-sm-12 mr-3  ">
+        
+          <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/cdc1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button type="button" class="btn btn-primary">Voir plus</button>
+  </div>
+</div>
+          </div>
+          <div class="col-md-3 mb-2 col-sm-12 mr-3">
+          <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/cdc2.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button type="button" class="btn btn-primary">Voir plus</button>
+  </div>
+</div>
+
+          </div>
+          <div class="col-md-3 mb-2 col-sm-12 mr-3">
+          <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/cdc3.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button type="button" class="btn btn-primary">Voir plus</button>
+  </div>
+</div>
+          
+
+
+
+
+
+        </div>
+
+
+       </div>
+         
+
+    </aside>
+
+
+    
+      
+        
+
+<!--footer-->
+<footer></footer>
+    <div class="site-section bg-dark">
+      <div class="container">
+
+      <div class="row text-white">
+        <div class="col-md-3">
+        <nav>
+          <ul>
+            <li> Condition générales d'utilisation </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="col-md-3">
+        <nav>
+          <ul>
+            <li>Vos informmations personnelles</li>
+          </ul>
+        </nav>
+      </div>
+      <div class="col-md-3">
+        <nav>
+          <ul>
+            <li>Cookies</li>
+          </ul>
+        </nav>
+      </div>
+      <div class="col-md-3">
+        <nav>
+          <ul>
+            <li>@Octobre 2024</li>
+          </ul>
+        </nav>
+      </div>
+          
+        </div>
+      </div>
+    </div>
+
+
+    
+
+  </div>  
+  </div>
+  </footer> <!--fin de faooter-->
+  <!--CDN bootstrap-->
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+  
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/mediaelement-and-player.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/circleaudioplayer.js"></script>
+
+  <script src="js/main.js"></script>
+
+    
+  </body>
+</html>
